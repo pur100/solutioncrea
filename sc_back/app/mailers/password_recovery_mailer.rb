b90e030167636a0afdc822e35cc0797693bdebac
Hugo MANCINI <h.g.mancini@gmail.com>
2020-10-22 12:36:17 +0200

    try to push

---
+++ sc_back/app/mailers/password_recovery_mailer.rb
@@ -0,0 +1,2 @@
+class PasswordRecoveryMailer < ApplicationMailer
+end
--- sc_back/config/environments/development.rb
+++ sc_back/config/environments/development.rb
@@ -27,6 +27,22 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentification: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"]
+    user_name: ENV["GMAIL_PASSWORD"]
+  }
+
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 end
